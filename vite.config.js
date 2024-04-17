bdb861dcde2d8ab741444796d42d6e94f1a6f2ff
noty-ivan <ivan.sakhnovskyi@noty.ai>
2024-04-17 23:41:08 +0300

    fix deploy build

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { compression } from 'vite-plugin-compression2';
 import { analyzer } from 'vite-bundle-analyzer';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE_PATH,
-  plugins: [vue(), compression(), analyzer({analyzerMode: 'server', analyzerPort: '8888'})],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.VITE_BASE_PATH,
+    plugins: [vue(), compression(), analyzer({ analyzerMode: 'server', analyzerPort: '8888' })],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    port: 3000,
-  },
+    server: {
+      port: 3000,
+    },
+  };
 });
