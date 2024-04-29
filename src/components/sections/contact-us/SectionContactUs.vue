8b90f72b5e58cd07905d2aad031c4fb98b3b2f91
noty-ivan <ivan.sakhnovskyi@noty.ai>
2024-04-29 14:18:15 +0300

    increase contact-us padding top

--- src/components/sections/contact-us/SectionContactUs.vue
+++ src/components/sections/contact-us/SectionContactUs.vue
@@ -4,7 +4,7 @@ import CallbackForm from './CallbackForm.vue';
 
 </script>
 <template>
-  <section class="bg-white flex flex-col md:flex-row px-3 pt-20 pb-4 sm:pb-10">
+  <section class="bg-white flex flex-col md:flex-row px-3 pt-36 pb-4 sm:pb-10">
     <TeamPhotos class="md:w-6/12" />
     <CallbackForm class="md:w-6/12" />
   </section>
